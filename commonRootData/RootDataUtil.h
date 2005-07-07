a4f70e734cdaa7a9420ae1b1d0fd326ae65ffdd9
chamont <>
2005-07-07 12:32:47 +0000

    add low level comparison methods

--- commonRootData/RootDataUtil.h
+++ commonRootData/RootDataUtil.h
@@ -1,11 +1,30 @@
+
 #ifndef ROOTDATAUTIL_H
 #define ROOTDATAUTIL_H
 
 #include "Exception.h"
 
+#include "Rtypes.h"
+class TVector3 ;
+class TLorentzVector ;
+
+#include <string>
+
 namespace rootdatautil {
+    
     extern void CheckBounds(long nIndex, long nLower, long nUpper);
     extern void CheckBufSize(unsigned long nBufSize, unsigned long nCorrectSize); 
+
+    // low level comparisons
+    bool Compare( Double_t, Double_t, const std::string & name ="unknown real" ) ;
+    bool Compare( Int_t, Int_t, const std::string & name ="unknown integer" ) ;
+    bool Compare( UInt_t, UInt_t, const std::string & name ="unknown integer" ) ;
+    bool Compare( Bool_t, Bool_t, const std::string & name ="unknown bool" ) ;
+    bool Compare( const std::string &, const std::string &, const std::string & name ="unknown vector3" ) ;
+    bool Compare( const TVector3 &, const TVector3 &, const std::string & name ="unknown vector3" ) ;
+    bool Compare( const TLorentzVector &, const TLorentzVector &, const std::string & name ="unknown lorentz vector" ) ;
+    bool Compare( void *, void *, const std::string & name ="unknown pointer" ) ;
+
 };
 
 #endif
--- src/RootDataUtil.cxx
+++ src/RootDataUtil.cxx
@@ -1,11 +1,89 @@
-#include "commonRootData/RootDataUtil.h"
 
-void rootdatautil::CheckBounds(long nIndex, long nLower, long nUpper) {
+#include "commonRootData/RootDataUtil.h"
+#include "TVector.h"
+#include "TLorentzVector.h"
+#include <iostream>
+
+namespace rootdatautil {
+    
+void CheckBounds(long nIndex, long nLower, long nUpper) {
 	if ((nIndex < nLower) || (nIndex > nUpper))
 		throw(IndexOutOfBoundsException(nIndex, nLower, nUpper));
 }
 
-void rootdatautil::CheckBufSize(unsigned long nBufSize, unsigned long nCorrectSize) {
+void CheckBufSize(unsigned long nBufSize, unsigned long nCorrectSize) {
 	if (nBufSize != nCorrectSize)
 		throw(BadBufferException(nBufSize, nCorrectSize));
+}
+
+
+//============================================================
+// For tests, compare with an acceptable error for floats.
+//============================================================
+
+template <class T>
+bool DirectCompareVals( T v1, T v2, const std::string & name ) {
+  if (v1==v2) return true ;
+  std::cout
+    <<"Comparison ERROR for "<<name<<": "
+    <<v1<<" != "<<v2
+    <<std::endl ;
+  return false ;
+}
+
+template <class T>
+bool DirectCompareRefs( const T & v1, const T & v2, const std::string & name ) {
+  if (v1==v2) return true ;
+  std::cout
+    <<"Comparison ERROR for "<<name<<": "
+    <<v1<<" != "<<v2
+    <<std::endl ;
+  return false ;
+}
+
+bool Compare( Double_t v1, Double_t v2, const std::string & name ) {
+  const Double_t fudge=0.00001 ;
+  if ((v1>=(v2-fudge))&&(v1<=(v2+fudge)) ){
+    return true ;
+  }
+  std::cout
+    <<"Comparison ERROR for "<<name<<": "
+    <<v1<<" != "<<v2
+    <<std::endl ;
+  return false ;
+}
+
+bool Compare( Int_t v1, Int_t v2, const std::string & name ) {
+    return DirectCompareVals(v1,v2,name) ;
+}
+ 
+bool Compare( UInt_t v1, UInt_t v2, const std::string & name ) {
+    return DirectCompareVals(v1,v2,name) ;
+}
+ 
+bool Compare( Bool_t v1, Bool_t v2, const std::string & name ) {
+    return DirectCompareVals(v1,v2,name) ;
+}
+ 
+bool Compare( const std::string & v1, const std::string & v2, const std::string & name ) {
+    return DirectCompareRefs(v1,v2,name) ;
+}
+ 
+bool Compare( const TVector3 & v1, const TVector3 & v2, const std::string & name ) {
+    return Compare(v1.X(),v2.X(),name+" X")
+        && Compare(v1.Y(),v2.Y(),name+" Y")
+        && Compare(v1.Z(),v2.Z(),name+" Z") ;
+}
+ 
+bool Compare( const TLorentzVector & v1, const TLorentzVector & v2, const std::string & name ) {
+    return Compare(v1.X(),v2.X(),name+" X")
+        && Compare(v1.Y(),v2.Y(),name+" Y")
+        && Compare(v1.Z(),v2.Z(),name+" Z")
+        && Compare(v1.T(),v2.T(),name+" T") ;
+}
+ 
+bool Compare( void * v1, void * v2, const std::string & name ) {
+    return DirectCompareVals(v1,v2,name) ;
+}
+ 
 }
