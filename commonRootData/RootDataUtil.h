b5a3b729d8a5158943a9121a007b31e01eb08fd7
chamont <>
2005-07-07 15:35:15 +0000

    Compare() is reserved by ROOT, replace with CompareInRange()

--- commonRootData/RootDataUtil.h
+++ commonRootData/RootDataUtil.h
@@ -16,14 +16,14 @@ namespace rootdatautil {
     extern void CheckBufSize(unsigned long nBufSize, unsigned long nCorrectSize); 
 
     // low level comparisons
-    bool Compare( Double_t, Double_t, const std::string & name ="unknown real" ) ;
-    bool Compare( Int_t, Int_t, const std::string & name ="unknown integer" ) ;
-    bool Compare( UInt_t, UInt_t, const std::string & name ="unknown integer" ) ;
-    bool Compare( Bool_t, Bool_t, const std::string & name ="unknown bool" ) ;
-    bool Compare( const std::string &, const std::string &, const std::string & name ="unknown vector3" ) ;
-    bool Compare( const TVector3 &, const TVector3 &, const std::string & name ="unknown vector3" ) ;
-    bool Compare( const TLorentzVector &, const TLorentzVector &, const std::string & name ="unknown lorentz vector" ) ;
-    bool Compare( void *, void *, const std::string & name ="unknown pointer" ) ;
+    bool CompareInRange( Double_t, Double_t, const std::string & name ="unknown real" ) ;
+    bool CompareInRange( Int_t, Int_t, const std::string & name ="unknown integer" ) ;
+    bool CompareInRange( UInt_t, UInt_t, const std::string & name ="unknown integer" ) ;
+    bool CompareInRange( Bool_t, Bool_t, const std::string & name ="unknown bool" ) ;
+    bool CompareInRange( const std::string &, const std::string &, const std::string & name ="unknown vector3" ) ;
+    bool CompareInRange( const TVector3 &, const TVector3 &, const std::string & name ="unknown vector3" ) ;
+    bool CompareInRange( const TLorentzVector &, const TLorentzVector &, const std::string & name ="unknown lorentz vector" ) ;
+    bool CompareInRange( void *, void *, const std::string & name ="unknown pointer" ) ;
 
 };
 
--- src/RootDataUtil.cxx
+++ src/RootDataUtil.cxx
@@ -22,7 +22,7 @@ void CheckBufSize(unsigned long nBufSize, unsigned long nCorrectSize) {
 //============================================================
 
 template <class T>
-bool DirectCompareVals( T v1, T v2, const std::string & name ) {
+bool DirectCompareInRangeVals( T v1, T v2, const std::string & name ) {
   if (v1==v2) return true ;
   std::cout
     <<"Comparison ERROR for "<<name<<": "
@@ -32,7 +32,7 @@ bool DirectCompareVals( T v1, T v2, const std::string & name ) {
 }
 
 template <class T>
-bool DirectCompareRefs( const T & v1, const T & v2, const std::string & name ) {
+bool DirectCompareInRangeRefs( const T & v1, const T & v2, const std::string & name ) {
   if (v1==v2) return true ;
   std::cout
     <<"Comparison ERROR for "<<name<<": "
@@ -41,7 +41,7 @@ bool DirectCompareRefs( const T & v1, const T & v2, const std::string & name ) {
   return false ;
 }
 
-bool Compare( Double_t v1, Double_t v2, const std::string & name ) {
+bool CompareInRange( Double_t v1, Double_t v2, const std::string & name ) {
   const Double_t fudge=0.00001 ;
   if ((v1>=(v2-fudge))&&(v1<=(v2+fudge)) ){
     return true ;
@@ -53,37 +53,37 @@ bool Compare( Double_t v1, Double_t v2, const std::string & name ) {
   return false ;
 }
 
-bool Compare( Int_t v1, Int_t v2, const std::string & name ) {
-    return DirectCompareVals(v1,v2,name) ;
+bool CompareInRange( Int_t v1, Int_t v2, const std::string & name ) {
+    return DirectCompareInRangeVals(v1,v2,name) ;
 }
  
-bool Compare( UInt_t v1, UInt_t v2, const std::string & name ) {
-    return DirectCompareVals(v1,v2,name) ;
+bool CompareInRange( UInt_t v1, UInt_t v2, const std::string & name ) {
+    return DirectCompareInRangeVals(v1,v2,name) ;
 }
  
-bool Compare( Bool_t v1, Bool_t v2, const std::string & name ) {
-    return DirectCompareVals(v1,v2,name) ;
+bool CompareInRange( Bool_t v1, Bool_t v2, const std::string & name ) {
+    return DirectCompareInRangeVals(v1,v2,name) ;
 }
  
-bool Compare( const std::string & v1, const std::string & v2, const std::string & name ) {
-    return DirectCompareRefs(v1,v2,name) ;
+bool CompareInRange( const std::string & v1, const std::string & v2, const std::string & name ) {
+    return DirectCompareInRangeRefs(v1,v2,name) ;
 }
  
-bool Compare( const TVector3 & v1, const TVector3 & v2, const std::string & name ) {
-    return Compare(v1.X(),v2.X(),name+" X")
-        && Compare(v1.Y(),v2.Y(),name+" Y")
-        && Compare(v1.Z(),v2.Z(),name+" Z") ;
+bool CompareInRange( const TVector3 & v1, const TVector3 & v2, const std::string & name ) {
+    return CompareInRange(v1.X(),v2.X(),name+" X")
+        && CompareInRange(v1.Y(),v2.Y(),name+" Y")
+        && CompareInRange(v1.Z(),v2.Z(),name+" Z") ;
 }
  
-bool Compare( const TLorentzVector & v1, const TLorentzVector & v2, const std::string & name ) {
-    return Compare(v1.X(),v2.X(),name+" X")
-        && Compare(v1.Y(),v2.Y(),name+" Y")
-        && Compare(v1.Z(),v2.Z(),name+" Z")
-        && Compare(v1.T(),v2.T(),name+" T") ;
+bool CompareInRange( const TLorentzVector & v1, const TLorentzVector & v2, const std::string & name ) {
+    return CompareInRange(v1.X(),v2.X(),name+" X")
+        && CompareInRange(v1.Y(),v2.Y(),name+" Y")
+        && CompareInRange(v1.Z(),v2.Z(),name+" Z")
+        && CompareInRange(v1.T(),v2.T(),name+" T") ;
 }
  
-bool Compare( void * v1, void * v2, const std::string & name ) {
-    return DirectCompareVals(v1,v2,name) ;
+bool CompareInRange( void * v1, void * v2, const std::string & name ) {
+    return DirectCompareInRangeVals(v1,v2,name) ;
 }
  
 }
