ab3f175fbeea4e4363e10fa0bd6f5f4d5b741f04
heather <>
2006-07-12 22:35:23 +0000

    update for new QdcHit

--- src/adf/QdcHit.cxx
+++ src/adf/QdcHit.cxx
@@ -13,14 +13,15 @@ QdcHit::QdcHit() {
     Clear();
 }
 
-QdcHit::QdcHit(UInt_t channel, UInt_t pulseHght, Bool_t isPedSubtracted) {
+QdcHit::QdcHit(UInt_t channel, UInt_t pulseHght, UInt_t module,
+               Bool_t isPedSubtracted) {
 
-    initialize(channel, pulseHght, isPedSubtracted);
+    initialize(channel, pulseHght, module, isPedSubtracted);
 }
 
 QdcHit::QdcHit(const QdcHit& copy):TObject(copy) {
 
-    initialize(copy.m_channel, copy.m_pulseHeight, copy.m_isPedestalSubtracted);
+    initialize(copy.m_channel, copy.m_pulseHeight, copy.m_module, copy.m_isPedestalSubtracted);
 }
 
 
@@ -28,21 +29,24 @@ QdcHit::~QdcHit() {
     Clear();  
 }
 
-void QdcHit::initialize(UInt_t channel, UInt_t pulseHgt, Bool_t isPedSubtracted) {
+void QdcHit::initialize(UInt_t channel, UInt_t pulseHgt, UInt_t module,
+                        Bool_t isPedSubtracted) {
 
     m_channel = channel;
     m_pulseHeight = pulseHgt;
+    m_module = module;
     m_isPedestalSubtracted = isPedSubtracted;
 }
 
 QdcHit& QdcHit::operator=(const QdcHit& copy) {
-    initialize(copy.m_channel, m_pulseHeight, copy.m_isPedestalSubtracted);
+    initialize(copy.m_channel, copy.m_pulseHeight, copy.m_module, copy.m_isPedestalSubtracted);
     return *this;
 }
 
 
 void QdcHit::Clear(Option_t *option) {
     m_channel = 0;
+    m_module = 0;
     m_pulseHeight = 0;
     m_isPedestalSubtracted = false;
 }
@@ -54,6 +58,7 @@ void QdcHit::Print(Option_t *option) const {
     cout << "QdcHit:" << endl;
     cout << "Channel: " << m_channel  
          << " PulseHeight: " << m_pulseHeight
+         << " Module: " << m_module
          << " isPedestalSubtracted: " << m_isPedestalSubtracted << endl;
     cout << dec;
 }
@@ -64,6 +69,7 @@ Bool_t QdcHit::CompareInRange( const QdcHit &ref, const std::string& name ) cons
     result = rootdatautil::CompareInRange(getChannel(),ref.getChannel(),"Channel") && result;
     result = rootdatautil::CompareInRange(getPulseHeight(),ref.getPulseHeight(),"PulseHeight") && result;
     result = rootdatautil::CompareInRange(isPedestalSubtracted(),ref.isPedestalSubtracted(),"IsPedestalSubtracted") && result;
+    result = rootdatautil::CompareInRange(getModule(), ref.getModule(),"Module") && result;
 
     if (!result) {
         if (name == "") {
