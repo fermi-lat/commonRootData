d534aa4708cf6379e0aaec973a67457ed53e32f1
usher <>
2007-02-15 19:32:14 +0000

    Updates for new relations in output stream

--- commonRootData/RelTable.h
+++ commonRootData/RelTable.h
@@ -10,7 +10,7 @@
 * @brief GLAST Relational Table class.
 *
 *  
-*  $Header: /nfs/slac/g/glast/ground/cvs/commonRootData/commonRootData/RelTable.h,v 1.1.1.1 2002/12/02 21:41:30 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/commonRootData/commonRootData/RelTable.h,v 1.1.1.1 2002/12/03 17:39:27 heather Exp $
 */
 
 class RelTable : public TObject  
@@ -22,6 +22,11 @@ public:
     RelTable(const RelTable& copy);
     
     virtual ~RelTable();
+
+    inline UInt_t getEventId() const { return m_eventId; };
+    inline UInt_t getRunId() const { return m_runId; };
+
+    void initialize(UInt_t nEvent, UInt_t nRun);
     
     /// clear lists, free pointers, etc., after read from / write to file
     void Clear(Option_t *option ="");
@@ -36,7 +41,13 @@ public:
     
     
 private:
+    /// unique event id for this run (for indexing)
+    UInt_t m_eventId;
     
+    /// Run number (for indexing)
+    UInt_t m_runId;
+
+    /// The list of relations that we really wwant
     TObjArray *m_table;
     
     ClassDef(RelTable,1) // Relational Table Class
