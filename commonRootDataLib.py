cc205c3c66acb702b2252b8abb8a4e3e8a491d97
jrb <>
2009-07-30 21:06:13 +0000

    SConscript, commonRootDataLib.py: small changes for SCons src/FileHeader.cxx:  add blank line

--- commonRootDataLib.py
+++ commonRootDataLib.py
@@ -1,7 +1,9 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/commonRootData/commonRootDataLib.py,v 1.1 2008/07/09 21:13:45 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['commonRootData'])
+
+    env.Tool('addLibrary', library = env['rootLibs'])
 
 def exists(env):
     return 1;
