7310984c0d818805d45bfbfd1e6f095ddc7f1b88
jrb <>
2012-08-16 23:53:48 +0000

    for Windows no-installed-headers

--- commonRootDataLib.py
+++ commonRootDataLib.py
@@ -1,7 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/commonRootData/commonRootDataLib.py,v 1.1 2008/07/09 21:13:45 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/commonRootData/commonRootDataLib.py,v 1.2 2009/07/30 21:06:13 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['commonRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'commonRootData') 
 
     env.Tool('addLibrary', library = env['rootLibs'])
 
